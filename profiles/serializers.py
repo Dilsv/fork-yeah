0314df396497a83ce6f5e308dc88d57f26b44c66
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-19 22:53:57 +0000

    Implement profile detail view and permissions.py for profile ownership

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,7 +3,19 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Profile model.
+    """
+
     owner = serializers.ReadOnlyField(source="owner.username")
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        """
+        Returns True if the authenticated user is the owner of the profile,
+        False otherwise.
+        """
+        return self.context["request"].user == obj.owner
 
     class Meta:
         model = Profile
@@ -13,6 +25,9 @@ class ProfileSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
             "name",
+            "content",
             "email_address",
             "image",
+            "is_owner",
         ]
+    
--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,26 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+
+from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
+from fork_yeah.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
-        return Response(serializer.data)
+class ProfileList(generics.ListCreateAPIView):
+    """
+    API view for listing and creating profiles.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a profile instance.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
