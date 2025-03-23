7019493037e18270c19a3727ad6fcd3776e4cba7
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-23 15:00:57 +0000

    Add roots, update settings and URLs for JWT cookie management

--- fork_yeah/serializers.py
+++ fork_yeah/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
 
@@ -5,8 +6,11 @@ from rest_framework import serializers
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
-    class Meta(UserDetailsSerializer.Meta):
-        fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
-        )
+
+class Meta(UserDetailsSerializer.Meta):
+    fields = UserDetailsSerializer.Meta.fields + (
+        'profile_id', 'profile_image'
+    )
