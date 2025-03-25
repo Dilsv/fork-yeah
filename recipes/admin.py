e1132250ca949e8cb4572eda3896ac33a0346dce
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-25 13:46:02 +0000

    Register Recipe, Ingredient, RecipeIngredient, and Category models in admin panel with slug prepopulation for Category

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient, Category
 
-# Register your models here.
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(RecipeIngredient)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
