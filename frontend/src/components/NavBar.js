83b78bad2844715426889a96033b516ee6741856
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-26 18:16:13 +0000

    Implement dynamic user authentication in NavBar with avatar display and update styles

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,8 +5,39 @@ import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/logo.png';
 import styles from '../styles/NavBar.module.css';
 import { NavLink } from 'react-router-dom';
+import { useCurrentUser } from '../contexts/CurrentUserContext';
+import Avatar from './Avatar';
 
 const NavBar = () => {
+    const currentUser = useCurrentUser();
+
+    const loggedInIcons = (
+        <>
+            <NavLink to="/favourites" className={styles.NavLink} activeClassName={styles.Active}>
+                <i className="fa-solid fa-star"><span>Favourites</span></i>
+            </NavLink>
+            <NavLink className={styles.NavLink} to="/" onClick={() => {}}>
+                <i className="fas fa-sign-out-alt"><span>Sign out</span></i>
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                to={`/profiles/${currentUser?.profile_id}`}
+            >
+                <Avatar src={currentUser?.profile_image} text={currentUser.username} height={50} />
+            </NavLink>
+        </>
+    );
+
+    const loggedOutIcons =
+        <>
+            <NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
+                <i className='fas fa-sign-in-alt'><span>Sign in</span></i>
+            </NavLink>
+            <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
+                <i className='fas fa-user-plus'><span>Sign up</span></i>
+            </NavLink>
+        </>
+
     return (
         <Navbar
             expand="md"
@@ -26,12 +57,11 @@ const NavBar = () => {
                 </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
+                    <Nav className="ms-auto align-items-center">
                         <NavLink exact to="/" className={styles.NavLink} activeClassName={styles.Active}><i className='fas fa-home'><span>Home</span></i></NavLink>
                         <NavLink to="/recipes" className={styles.NavLink} activeClassName={styles.Active}><i className='fa-solid fa-bowl-food'><span>Recipes</span></i></NavLink>
-                        <NavLink to="/favourites" className={styles.NavLink} activeClassName={styles.Active}><i className="fa-solid fa-star"><span>Favourites</span></i></NavLink>
-                        <NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}><i className='fas fa-sign-in-alt'><span>Sign in</span></i></NavLink>
-                        <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}><i className='fas fa-user-plus'><span>Sign up</span></i></NavLink>
+                        {currentUser ? loggedInIcons : loggedOutIcons}
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
