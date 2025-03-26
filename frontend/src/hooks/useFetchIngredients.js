2499806d3c80f0422f8984fc17c505f05bd90bfd
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-26 20:49:25 +0000

    Add state management for addedIngredient in useFetchIngredients hook

--- frontend/src/hooks/useFetchIngredients.js
+++ frontend/src/hooks/useFetchIngredients.js
@@ -3,19 +3,21 @@ import { axiosReq } from '../api/axiosDefault.js';
 
 const useFetchIngredients = () => {
     const [ingredients, setIngredients] = useState([]);
+    const [addedIngredient, setAddedIngredient] = useState(false);
 
     useEffect(() => {
         const fetchIngredients = async () => {
             try {
                 const response = await axiosReq.get("/ingredients/");
                 setIngredients(response.data.results);
+                setAddedIngredient(false);
             } catch (error) {
                 console.error("Error fetching ingredients", error);
             }
         };
         fetchIngredients();
-    }, []);
-    return { ingredients, setIngredients };
+    }, [addedIngredient]);
+    return { ingredients, setIngredients, addedIngredient, setAddedIngredient };
 }
 
 export default useFetchIngredients;
