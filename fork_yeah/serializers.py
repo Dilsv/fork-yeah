5e81f230cccbaa4a46b90be6df91410505f9ac05
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-26 17:15:44 +0000

    Refactor CurrentUserSerializer to improve field organization and readability

--- fork_yeah/serializers.py
+++ fork_yeah/serializers.py
@@ -6,11 +6,9 @@ from rest_framework import serializers
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
-    created_at = serializers.SerializerMethodField()
-    updated_at = serializers.SerializerMethodField()
 
 
-class Meta(UserDetailsSerializer.Meta):
-    fields = UserDetailsSerializer.Meta.fields + (
-        'profile_id', 'profile_image'
-    )
+    class Meta(UserDetailsSerializer.Meta):
+        fields = UserDetailsSerializer.Meta.fields + (
+            'profile_id', 'profile_image'
+        )
