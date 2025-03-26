bf757cfa1033b3a210f88cce44ce67ad3bf7dbdb
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-26 13:32:57 +0000

    Refactor RecipeSerializer to change author to owner, add is_owner field, and implement custom create method for handling ingredients

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -26,15 +26,19 @@ class RecipeIngredientSerializer(serializers.ModelSerializer):
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    # Only display author's username
-    author = serializers.ReadOnlyField(source="author.username")
+    owner = serializers.ReadOnlyField(source="owner.username")
+    is_owner = serializers.SerializerMethodField()
     ingredients = RecipeIngredientSerializer(
         source="recipe_ingredients", many=True,
         required=False,
         read_only=True
-    )  # Nested ingredients
+    )
     category = serializers.SlugRelatedField(
         slug_field="name", queryset=Category.objects.all())
+
+    def get_is_owner(self, obj):
+        request = self.context["request"]
+        return obj.owner == request.user
 
     class Meta:
         model = Recipe
@@ -45,8 +49,44 @@ class RecipeSerializer(serializers.ModelSerializer):
             "instructions",
             "image",
             "category",
-            "author",
+            "owner",
             "ingredients",
             "created_at",
             "updated_at",
+            "approved",
+            "is_owner",
         ]
+
+    def create(self, validated_data):
+        ingredients_data = self.context["request"].data.get("ingredients", [])
+
+        if isinstance(ingredients_data, str):
+            raise serializers.ValidationError(
+                {"ingredients": "This field must be a list"}
+            )
+
+        # Create and save the Recipe first
+        recipe = Recipe.objects.create(**validated_data)
+
+        # Add ingredients to the recipe
+        for ing in ingredients_data:
+            recipe = Recipe.objects.get(id=recipe.id)
+            ingredient_id = ing.get("ingredient")
+            quantity = ing.get("quantity")
+            unit = ing.get("unit")
+
+            try:
+                ingredient = Ingredient.objects.get(id=ingredient_id)
+                RecipeIngredient.objects.create(
+                    recipe=recipe,  # Recipe instance ID
+                    ingredient=ingredient,
+                    quantity=quantity,
+                    unit=unit
+                )
+            except Ingredient.DoesNotExist:
+                raise serializers.ValidationError(
+                    {"ingredient": f"Invalid ingredient ID: {ingredient_id}"}
+                )
+
+        return recipe
+
