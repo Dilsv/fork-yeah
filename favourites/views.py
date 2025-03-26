ec529a10bc528c955eac247b2994b406ba91d1e6
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-26 22:26:50 +0000

    Refactor favourites views: rename classes, update permissions, and adjust queryset handling

--- favourites/views.py
+++ favourites/views.py
@@ -1,22 +1,25 @@
 from rest_framework import generics, permissions
 from .models import Favourite
 from .serializers import FavouriteSerializer
+from fork_yeah.permissions import IsOwnerOrReadOnly
 
 
-class FavouriteListCreateView(generics.ListCreateAPIView):
+class FavouritesList(generics.ListCreateAPIView):
+    """
+    API view for listing and creating Favourites.
+    """
+    queryset = Favourite.objects.all()
     serializer_class = FavouriteSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        return Favourite.objects.filter(user=self.request.user)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        serializer.save(owner=self.request.user)
 
 
-class FavouriteDeleteView(generics.DestroyAPIView):
+class FavouritesDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve or delete a specific Favourite item.
+    """
+    queryset = Favourite.objects.all()
     serializer_class = FavouriteSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        return Favourite.objects.filter(user=self.request.user)
+    permission_classes = [IsOwnerOrReadOnly]
