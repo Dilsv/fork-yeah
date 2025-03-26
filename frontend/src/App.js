307a47e5ac6b9707ee33134ca7d55410c3601491
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-26 16:45:56 +0000

    Add SignUpForm component and update routing in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import Container from 'react-bootstrap/esm/Container';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Route, Switch } from 'react-router-dom';
-import NotFound from './pages/errors/NotFound';
 import "./api/axiosDefault.js";
+import SignUpForm from './pages/auth/SignUpForm.js';
 
 function App() {
   return (
@@ -13,11 +13,9 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home Page</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/recipes" render={() => <h1>Recipes</h1>} />
           <Route exact path="/favourites" render={() => <h1>Favourites</h1>} />
-          <Route render={() => <NotFound /> } />
-
 
         </Switch>
       </Container>
