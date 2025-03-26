817987b7fa4666672971fcc10a4bc1694b57159e
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-26 15:44:52 +0000

    Add routes for Recipes and Favourites pages in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import styles from "./App.module.css";
+import Container from 'react-bootstrap/esm/Container';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -13,6 +12,8 @@ function App() {
           <Route exact path="/" render={() => <h1>Home Page</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
           <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route exact path="/recipes" render={() => <h1>Recipes</h1>} />
+          <Route exact path="/favourites" render={() => <h1>Favourites</h1>} />
 
         </Switch>
       </Container>
