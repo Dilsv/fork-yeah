acfcec8de967ab970023797f979fdc074689540c
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-26 14:25:02 +0000

    Add NavBar component and integrate it into App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.module.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <NavBar />
     </div>
   );
 }
