45ec59cffbde0e4532a659a62539e83301ee7cc7
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-26 22:42:28 +0000

    Add RecipePage component to fetch and display recipe details

---
+++ frontend/src/pages/recipes/RecipePage.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react"
+import { useHistory, useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefault.js";
+import { Recipe } from "./Recipe";
+
+export const RecipePage = () => {
+    const { id } = useParams();
+    const history = useHistory();
+    const [recipe, setRecipe] = useState({ results: [] });
+
+    useEffect(() => {
+        // Fetch the recipe from the API.
+        const handleMount = async () => {
+            try {
+                const [{ data: recipe }] = await Promise.all([
+                    axiosReq.get(`/recipes/${id}/`),
+                ]);
+                setRecipe({ results: [recipe] });
+            } catch (err) {
+                if (err.response.status === 404) {
+                    history.push("/notfound");
+                }
+                console.log(err);
+            }
+        };
+        handleMount();
+    }, [id, history]);
+
+
+    return (
+        <>
+            <Recipe {...recipe.results[0]} setRecipe={setRecipe} recipePage />
+        </>
+    )
+}
