a2aa6a7607b293d48c3d116708c7b42ba78afa7e
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-26 16:49:50 +0000

    Add SignInForm component and update routing in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar';
 import { Route, Switch } from 'react-router-dom';
 import "./api/axiosDefault.js";
 import SignUpForm from './pages/auth/SignUpForm.js';
+import SignInForm from './pages/auth/SignInForm';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Container fluid className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home Page</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/recipes" render={() => <h1>Recipes</h1>} />
           <Route exact path="/favourites" render={() => <h1>Favourites</h1>} />
