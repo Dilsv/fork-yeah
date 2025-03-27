d56e8c173201adf7e4fad71a7fea2d869ea5d2ce
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-27 09:52:24 +0000

    Enhance useFetchIngredients hook to support search functionality and loading state

--- frontend/src/hooks/useFetchIngredients.js
+++ frontend/src/hooks/useFetchIngredients.js
@@ -2,22 +2,25 @@ import { useEffect, useState } from 'react'
 import { axiosReq } from '../api/axiosDefault.js';
 
 const useFetchIngredients = () => {
-    const [ingredients, setIngredients] = useState([]);
+    const [ingredients, setIngredients] = useState({ results: [] });
     const [addedIngredient, setAddedIngredient] = useState(false);
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const [query, setQuery] = useState("");
 
     useEffect(() => {
         const fetchIngredients = async () => {
             try {
-                const response = await axiosReq.get("/ingredients/");
-                setIngredients(response.data.results);
+                const { data } = await axiosReq.get(`/ingredients/?search=${query}`);
+                setIngredients(data);
+                setHasLoaded(true);
                 setAddedIngredient(false);
             } catch (error) {
                 console.error("Error fetching ingredients", error);
             }
         };
         fetchIngredients();
-    }, [addedIngredient]);
-    return { ingredients, setIngredients, addedIngredient, setAddedIngredient };
+    }, [addedIngredient, query]);
+    return { ingredients, setIngredients, addedIngredient, setAddedIngredient, hasLoaded, setHasLoaded, query, setQuery };
 }
 
 export default useFetchIngredients;
