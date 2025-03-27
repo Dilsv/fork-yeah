0f8a03a048295222c97deb0dbb3b60441c9147ed
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-27 11:12:34 +0000

    Add ProfileEditForm component for editing user profiles

---
+++ frontend/src/pages/profiles/ProfileEditForm.js
@@ -0,0 +1,182 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useHistory, useParams } from "react-router-dom";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+
+import { axiosReq } from "../../api/axiosDefault.js";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../../contexts/CurrentUserContext";
+
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
+const ProfileEditForm = () => {
+  /**
+   * The ProfileEditForm component is a functional component that renders a form for editing the user's profile.
+   * It fetches the user's profile data from the API and updates the user's profile data.
+   * @returns {JSX.Element} - The JSX for the component.
+   */
+
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+  const { id } = useParams();
+  const history = useHistory();
+  const imageInput = useRef();
+  const profileDataSet = useRef(false);
+
+  const [profileData, setProfileData] = useState({
+    name: "",
+    image: "",
+  });
+  const { name, image } = profileData;
+
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    const handleMount = async () => {
+      // redirect user if they are not the owner of this profile
+      if (currentUser?.profile_id?.toString() === id) {
+        try {
+          // fetch the user's profile data from the API
+          const { data } = await axiosReq.get(`/profiles/${id}/`);
+          const { name, image } = data;
+          // set the profile data state if it has not been set
+          if (!profileDataSet.current) {
+            setProfileData({
+              name,
+              image,
+            });
+            profileDataSet.current = true;
+          }
+        } catch (err) {
+          // console.log(err);
+          history.push("/");
+        }
+      } else {
+        history.push("/");
+      }
+    };
+
+    handleMount();
+  }, [history, id, currentUser]);
+
+  const handleChange = (event) => {
+    setProfileData({
+      ...profileData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    if (imageInput?.current?.files[0]) {
+      formData.append("image", imageInput.current.files[0]);
+    }
+
+    try {
+      const { data } = await axiosReq.put(`/profiles/${id}/`, formData);
+      setCurrentUser((currentUser) => ({
+        ...currentUser,
+        profile_image: data.image,
+      }));
+      history.goBack();
+    } catch (err) {
+      // console.log(err);
+      setErrors(err.response?.data);
+    }
+  };
+
+  const textFields = (
+    <>
+      <Form.Group>
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          type="text"
+          value={name}
+          onChange={handleChange}
+          name="name"
+        />
+      </Form.Group>
+      {errors?.name?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Bright} mr-3`}
+        onClick={() => history.goBack()}
+      >
+        cancel
+      </Button>
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Bright}`}
+        type="submit"
+      >
+        save
+      </Button>
+    </>
+  );
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col className="py-2 p-0 p-md-2 text-center" md={7} lg={6}>
+          <Container className={appStyles.Content}>
+            <Form.Group>
+              {image && (
+                <figure>
+                  <Image src={image} fluid />
+                </figure>
+              )}
+              {errors?.image?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
+              <div>
+                <Form.Label
+                  className={`${btnStyles.Button} ${btnStyles.Bright} btn my-auto`}
+                  htmlFor="image-upload"
+                >
+                  Change the image
+                </Form.Label>
+              </div>
+              <input
+                hidden
+                type="file"
+                id="image-upload"
+                ref={imageInput}
+                accept="image/*"
+                onChange={(e) => {
+                  if (e.target.files.length) {
+                    setProfileData({
+                      ...profileData,
+                      image: URL.createObjectURL(e.target.files[0]),
+                    });
+                  }
+                }}
+              />
+            </Form.Group>
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={6} className="d-none d-md-block p-0 p-md-2 text-center">
+          <Container className={appStyles.Content}>{textFields}</Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default ProfileEditForm;
