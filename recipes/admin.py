cd8af38e5492c03b91f13c9e68872de6392cebce
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-27 02:10:09 +0000

    Enhance admin interface for Recipe model with custom admin actions and improved display options

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,11 +1,31 @@
 from django.contrib import admin
 from .models import Recipe, Ingredient, RecipeIngredient, Category
 
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
-admin.site.register(RecipeIngredient)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'updated_at', 'approved')
+    list_filter = (
+        'title',
+        'category',
+        'created_at',
+        'updated_at',
+        'approved')
+    search_fields = ('title', 'category__name')
+
+    actions = ['approve_recipes', 'disapprove_recipes']
+
+    def approve_recipes(self, request, queryset):
+        queryset.update(approved=True)
+
+    def disapprove_recipes(self, request, queryset):
+        queryset.update(approved=False)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Ingredient)
+admin.site.register(RecipeIngredient)
+admin.site.register(Recipe, RecipeAdmin)
