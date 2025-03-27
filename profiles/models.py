4592fc97a56edb5fac35bb82f5bf989b63dd8808
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-27 17:57:55 +0000

    Update default profile image path in Profile model and migration

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../mrwrqyzbn91lbdd8iy0q'
+        upload_to='images/', default='../default-profile_xku4qj'
     )
 
     class Meta:
