e0026f20594349dbd34f29a1d686d9c974454107
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-27 02:03:26 +0000

    Add fetchMoreData utility function to handle pagination and deduplicate results

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,4 +1,5 @@
 import { jwtDecode } from "jwt-decode";
+import { axiosReq } from "../api/axiosDefault.js";
 
 
 export const setTokenTimestamp = (data) => {
@@ -12,4 +13,19 @@ export const shouldRefreshToken = () => {
 
 export const removeTokenTimestamp = () => {
     localStorage.removeItem("refreshTokenTimestamp");
+};
+
+export const fetchMoreData = async (resource, setResource) => {
+    try {
+        const { data } = await axiosReq.get(resource.next);
+        setResource((prevResource) => ({
+            ...prevResource,
+            next: data.next,
+            results: data.results.reduce((acc, cur) => {
+                return acc.some((accResult) => accResult.id === cur.id)
+                    ? acc
+                    : [...acc, cur];
+            }, prevResource.results),
+        }));
+    } catch (err) { }
 };
