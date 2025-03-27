3b7943cdb6c177131dc98021fb856fd6a940df7c
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-27 11:18:40 +0000

    Add RecipeIngredientInline and IngredientAdmin to improve admin interface

--- recipes/admin.py
+++ recipes/admin.py
@@ -21,11 +21,22 @@ class RecipeAdmin(admin.ModelAdmin):
         queryset.update(approved=False)
 
 
+class RecipeIngredientInline(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'quantity', 'unit')
+    list_filter = ('recipe', 'ingredient', 'quantity', 'unit')
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
 
-admin.site.register(Ingredient)
-admin.site.register(RecipeIngredient)
+admin.site.register(RecipeIngredient, RecipeIngredientInline)
 admin.site.register(Recipe, RecipeAdmin)
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    search_fields = ('name',)
+
+
+admin.site.register(Ingredient, IngredientAdmin)
