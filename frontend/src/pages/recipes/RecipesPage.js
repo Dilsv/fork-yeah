4d7d411e457a35c1d0ca614c0606115a21c2bdc4
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-27 02:03:12 +0000

    Implement infinite scroll for recipe display on RecipesPage and replace Image component with Card.Img

--- frontend/src/pages/recipes/RecipesPage.js
+++ frontend/src/pages/recipes/RecipesPage.js
@@ -3,7 +3,6 @@ import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import Image from "react-bootstrap/Image";
 import { useLocation } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefault.js";
 import Asset from "../../components/Asset";
@@ -11,6 +10,8 @@ import Form from "react-bootstrap/Form";
 import styles from "../../styles/RecipesPage.module.css";
 import useFetchCategories from "../../hooks/useFetchCategories.js";
 import appStyles from "../../App.module.css";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from '../../utils/utils';
 
 const RecipesPage = () => {
   const [recipes, setRecipes] = useState([{ results: [] }]);
@@ -70,24 +71,32 @@ const RecipesPage = () => {
 
       {hasLoaded ? (
         recipes.results.length ? (
-          <Row className="justify-content-around">
-            {recipes.results.map((recipe) => (
-              <Col key={recipe.id} className="mb-2" md={4}>
-                <Card>
-                  <Card.Title className="text-center pt-1">
-                    {recipe.title}
-                  </Card.Title>
-                  <Image
-                    src={recipe.image}
-                    alt={recipe.title}
-                    className={`${appStyles.Image}`}
-                    height={200}
-                  />
-                  <Card.Footer>{recipe.category}</Card.Footer>
-                </Card>
-              </Col>
-            ))}
-          </Row>
+          <InfiniteScroll
+            dataLength={recipes.results.length}
+            next={() => fetchMoreData(recipes, setRecipes)}
+            hasMore={!!recipes.next}
+            loader={<Asset spinner />}
+          >
+            <Row className="justify-content-start align-items-center">
+              {recipes.results.map((recipe) => (
+                <Col key={recipe.id} className="mb-2" md={4}>
+                  <Card>
+                    <Card.Title className="text-center pt-1">
+                      {recipe.title}
+                    </Card.Title>
+                    <Card.Img
+                      src={recipe.image}
+                      alt={recipe.title}
+                      className={`${appStyles.Image}`}
+                      height={200}
+                    />
+                    <Card.Footer>{recipe.category}</Card.Footer>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </InfiniteScroll>
+
         ) : (
           <Asset message="No recipes found" />
         )
