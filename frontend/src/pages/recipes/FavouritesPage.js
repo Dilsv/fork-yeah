3637a0b965f6f34520096becb41fc8d43e2e200a
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-27 09:30:11 +0000

    Refactor FavouritesPage to utilize RecipesPageComponent for improved readability and functionality

--- frontend/src/pages/recipes/FavouritesPage.js
+++ frontend/src/pages/recipes/FavouritesPage.js
@@ -1,21 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefault.js";
-import Asset from "../../components/Asset";
-import Form from "react-bootstrap/Form";
-import styles from "../../styles/RecipesPage.module.css";
 import useFetchCategories from "../../hooks/useFetchCategories";
-import appStyles from "../../App.module.css";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { fetchMoreData } from "../../utils/utils";
 import useFetchFavourites from "../../hooks/useFetchFavourites";
+import RecipesPageComponent from "../../components/RecipesPageComponent";
 
 const RecipesPage = (props) => {
-  const [recipes, setRecipes] = useState([{ results: [] }]);
+  const [recipes, setRecipes] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
   const [query, setQuery] = useState("");
@@ -23,6 +15,7 @@ const RecipesPage = (props) => {
   const { categories } = useFetchCategories();
   const [category, setCategory] = useState("");
   const { recipeId } = useFetchFavourites(props);
+  const [favouritesArray, setFavouritesArray] = useState({ results: [] });
 
   useEffect(() => {
     const fetchRecipes = async () => {
@@ -31,6 +24,9 @@ const RecipesPage = (props) => {
           `/recipes/?search=${query}&category=${category}`
         );
         setRecipes(data);
+        setFavouritesArray(
+          data?.results?.filter((recipe) => recipeId.includes(recipe.id))
+        );
         setHasLoaded(true);
       } catch (error) {
         console.error(error);
@@ -44,78 +40,21 @@ const RecipesPage = (props) => {
     return () => {
       clearTimeout(timer);
     };
-  }, [query, pathname, category]);
-
-  const favouritesArray = recipes?.results?.filter((recipe) =>
-    recipeId.includes(recipe.id)
-  );
+  }, [query, pathname, category, recipeId]);
 
   return (
     <Container className="mt-5">
-      <Form className={styles.SearchBar} onSubmit={(e) => e.preventDefault()}>
-        <Form.Group className="mb-3 d-flex align-items-center justify-content-between">
-          <i className={`fas fa-search ${styles.SearchIcon}`} />
-          <Form.Control
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            type="text"
-            placeholder="Search"
-            className="me-1"
-          />
-          <Form.Select
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            className="text-capitalize ms-auto w-25"
-          >
-            <option value="">All categories </option>
-            {categories.map((category) => (
-              <option key={category.id} value={category.id}>
-                {category.name}
-              </option>
-            ))}
-          </Form.Select>
-        </Form.Group>
-      </Form>
-      <hr />
-
-      {hasLoaded ? (
-        favouritesArray?.length ? (
-          <InfiniteScroll
-            dataLength={favouritesArray.length}
-            next={() => fetchMoreData(recipes, setRecipes)}
-            hasMore={!!recipes.next}
-            loader={!hasLoaded && <Asset spinner />}
-          >
-            <Row className="justify-content-start align-items-center gx-2 mx-0">
-              {favouritesArray.map((recipe) => (
-                <Col key={recipe.id} className="mb-2" md={4}>
-                  <Link
-                    to={`/recipes/${recipe.id}`}
-                    className="text-decoration-none"
-                  >
-                    <Card>
-                      <Card.Title className="text-center pt-1">
-                        {recipe.title}
-                      </Card.Title>
-                      <Card.Img
-                        src={recipe.image}
-                        alt={recipe.title}
-                        className={`${appStyles.Image} pb-1`}
-                        height={200}
-                      />
-                      <Card.Footer>{recipe.category}</Card.Footer>
-                    </Card>
-                  </Link>
-                </Col>
-              ))}
-            </Row>
-          </InfiniteScroll>
-        ) : (
-          <Asset message="No recipes found" />
-        )
-      ) : (
-        <Asset spinner message="Loading recipes..." />
-      )}
+      <RecipesPageComponent
+        query={query}
+        setQuery={setQuery}
+        category={category}
+        setCategory={setCategory}
+        recipes={recipes}
+        setRecipes={setRecipes}
+        hasLoaded={hasLoaded}
+        categories={categories}
+        array={favouritesArray}
+      />
     </Container>
   );
 };
