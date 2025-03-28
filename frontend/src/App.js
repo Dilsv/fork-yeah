3dc587309ea67e7e00f5d3e3a47554cbd40a002f
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-03-28 19:15:42 +0000

    Add HomePage and Footer components; update App layout and styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
-import Container from 'react-bootstrap/esm/Container';
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
-import { Route, Switch } from 'react-router-dom';
+import Container from "react-bootstrap/esm/Container";
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefault.js";
-import SignUpForm from './pages/auth/SignUpForm.js';
-import SignInForm from './pages/auth/SignInForm';
-import CreateRecipeForm from './pages/recipes/CreateRecipeForm';
-import RecipePage from './pages/recipes/RecipePage';
-import RecipesPage from './pages/recipes/RecipesPage.js';
-import FavouritesPage from './pages/recipes/FavouritesPage';
-import UserRecipes from './pages/recipes/UserRecipes';
-import EditRecipeForm from './pages/recipes/EditRecipeForm';
-import ProfilePage from './pages/profiles/ProfilePage';
-import ProfileEditForm from './pages/profiles/ProfileEditForm';
-import UsernameForm from './pages/profiles/UsernameForm';
-import UserPasswordForm from './pages/profiles/UserPassword';
-import NotFound from './pages/errors/NotFound';
-
+import SignUpForm from "./pages/auth/SignUpForm.js";
+import SignInForm from "./pages/auth/SignInForm";
+import CreateRecipeForm from "./pages/recipes/CreateRecipeForm";
+import RecipePage from "./pages/recipes/RecipePage";
+import RecipesPage from "./pages/recipes/RecipesPage.js";
+import FavouritesPage from "./pages/recipes/FavouritesPage";
+import UserRecipes from "./pages/recipes/UserRecipes";
+import EditRecipeForm from "./pages/recipes/EditRecipeForm";
+import ProfilePage from "./pages/profiles/ProfilePage";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPassword";
+import NotFound from "./pages/errors/NotFound";
+import HomePage from "./pages/home/HomePage";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
@@ -24,24 +25,45 @@ function App() {
       <NavBar />
       <Container fluid className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route exact path="/" render={() => <HomePage />} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/recipes" render={() => <RecipesPage />} />
-          <Route exact path="/recipes/create" render={() => <CreateRecipeForm />} />
+          <Route
+            exact
+            path="/recipes/create"
+            render={() => <CreateRecipeForm />}
+          />
           <Route exact path="/recipes/:id" render={() => <RecipePage />} />
-          <Route exact path="/recipes/:id/edit" render={() => <EditRecipeForm />} />
+          <Route
+            exact
+            path="/recipes/:id/edit"
+            render={() => <EditRecipeForm />}
+          />
           <Route exact path="/my-recipes" render={() => <UserRecipes />} />
-          <Route exact path="/favourites" render={() => <FavouritesPage /> } />
+          <Route exact path="/favourites" render={() => <FavouritesPage />} />
           <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
-          <Route exact path="/profiles/:id/edit" render={() => <ProfileEditForm />} />
-          <Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />} />
-          <Route exact path="/profiles/:id/edit/password" render={() => <UserPasswordForm />} />
+          <Route
+            exact
+            path="/profiles/:id/edit"
+            render={() => <ProfileEditForm />}
+          />
+          <Route
+            exact
+            path="/profiles/:id/edit/username"
+            render={() => <UsernameForm />}
+          />
+          <Route
+            exact
+            path="/profiles/:id/edit/password"
+            render={() => <UserPasswordForm />}
+          />
 
           <Route render={() => <NotFound />} />
-
         </Switch>
       </Container>
+      <Footer />
+
     </div>
   );
 }
